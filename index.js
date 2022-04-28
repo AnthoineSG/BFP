b0554dbda1e8d5fe68cf6d0c3a2be8e6f1edc586
AnthoineSG <anthoine93@outlook.fr>
2022-04-28 18:35:39 +0200

    1er start du server

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("elo");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
