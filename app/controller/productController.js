33beefcbfdca62070cfa82ac70afa5c85420f81e
AnthoineSG <anthoine93@outlook.fr>
2022-05-01 16:22:19 +0200

    commit --amend j'ai du mal :(

--- app/controller/productController.js
+++ app/controller/productController.js
@@ -1,5 +1,5 @@
 // ! on importe les fichiers (../models/index.js) permetant la connection a la bdd grace a sequelize
-const { Product, Category, Function } = require("../models");
+const { Product, Function } = require("../models");
 
 async function getAllProduct(req, res) {
     // ^ on recupere tout les produit grace a la methode sequelize findAll qui fait (SELECT * FROM "product";)
