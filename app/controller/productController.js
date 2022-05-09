39ef2005cf829fe213fc4d786ea410b7b7bb9ee3
AnthoineSG <anthoine93@outlook.fr>
2022-05-09 20:35:54 +0200

    ajout d'une route pour ajouter un produit mais pas fini ^^'

--- app/controller/productController.js
+++ app/controller/productController.js
@@ -1,11 +1,11 @@
 // ! on importe les fichiers (../models/index.js) permetant la connection a la bdd grace a sequelize
-const { Product } = require("../models");
+const { Product, User, Place, Category, Function } = require("../models");
 
 async function getAllProduct(req, res) {
     try {
         // ^ on recupere tout les produit grace a la methode sequelize findAll qui fait (SELECT * FROM "product";)
         const allProduct = await Product.findAll();
-    
+
         res.render("product", { // & on passe nos resulta a la views
             products: allProduct,
         });
@@ -44,9 +44,61 @@ async function productHasFunction(req, res) {
     }
 }
 
+async function form(req, res) {
+    try {
+        const users = await User.findAll();
+        const places = await Place.findAll();
+        const categorys = await Category.findAll();
+        const fonctions = await Function.findAll();
+        res.render("productAdd", { users, places, categorys, fonctions });
+    } catch (error) {
+        console.error(error);
+    }
+}
+async function addProduct(req, res) {
+    try {
+        const { name, user, place, fonction, category } = req.body;
+
+        const newProduct = new Product({
+            name: name,
+            user_id: user,
+            function_id: fonction,
+        });
+        await newProduct.save();
+
+        // const lastProduct = await Product.findOne({
+        //     order: [["id", "DESC"]],
+        // });
+
+        // const newProductPlace = new product_has_place({
+        //     place_id: place,
+        //     product_id: lastProduct.id,
+        // });
+
+        // console.log(newProductPlace);
+
+
+        const users = await User.findAll();
+        const places = await Place.findAll();
+        const categorys = await Category.findAll();
+        const fonctions = await Function.findAll();
+        res.render("productAdd", { users, places, categorys, fonctions });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
+
+
 // ~ les fichier exporter vont au router
 module.exports = {
     getAllProduct,
     getOneProduct,
     productHasFunction,
+    form,
+    addProduct,
 };
--- app/route/routes/product.js
+++ app/route/routes/product.js
@@ -9,4 +9,7 @@ router.get("/product/:id", productController.getOneProduct);
 
 router.get("/product/sertpour/function", productController.productHasFunction);
 
+router.get("/product/add/new", productController.form);
+router.post("/product/add/new", productController.addProduct);
+
 module.exports = router;
